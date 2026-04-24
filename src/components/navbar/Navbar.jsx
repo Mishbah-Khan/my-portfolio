c42ad583d58a0ce0ed77022b84fe2106b58a817a
Mishbah Khan <mishbahkhan505@gmail.com>
2026-04-25 05:22:09 +0600

    add  home to nav list

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,6 +5,7 @@ const Navbar = () => {
     <nav>
       <div className="nav-logo">Mishbah<span>.</span>dev</div>
       <ul className="nav-links">
+        <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#projects">Projects</a></li>
         <li><a href="#experience">Experience</a></li>
