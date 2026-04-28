f0bb5b13b604b479d92289889f6546d4fbeac7dc
Mishbah Khan <mishbahkhan505@gmail.com>
2026-04-29 05:57:56 +0600

    add navbar image

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,12 @@ import './Navbar.css';
 const Navbar = () => {
   return (
     <nav>
-      <div className="nav-logo">Mishbah<span>.</span>dev</div>
+      <div className="nav-logo">
+        <img src="./logo.png" alt="logo" />
+        <span className="brand-text">
+          Mishbah<span>.</span>dev
+        </span>
+      </div>
       <ul className="nav-links">
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
